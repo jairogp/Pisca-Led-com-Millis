84a8fd7dc183e89f6bc3a736a402fb07ba221af7
jairogp <jairogp@users.noreply.github.com>
2017-07-16 07:37:44 -0300

    Add files via upload

---
+++ Pisca_Led_com_Millis/Pisca_Led_com_Millis.ino
@@ -0,0 +1,26 @@
+
+#define pinLed1 13
+
+void setup() {
+  pinMode(pinLed1, OUTPUT);
+  }
+
+void controlaLed1() {
+static unsigned long delayPisca;  
+
+   if ( (millis() - delayPisca) < 200 ) {
+      digitalWrite(pinLed1, HIGH);
+  } else {
+      digitalWrite(pinLed1, LOW);
+  }
+
+  if ( (millis() - delayPisca) >= 2000 ) {
+    delayPisca = millis(); 
+  }
+  }
+
+void loop() {
+  controlaLed1();
+  }
+
+
